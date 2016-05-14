0dfcc04f71d5114eadf2d1593a1bcd91a304c00a
javier <javier@javiers-Mac-Pro.local>
2016-05-13 18:58:58 -0700

    Initial Commit

---
+++ PruebaRunator/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PruebaRunator
+//
+//  Created by javier on 5/13/16.
+//  Copyright (c) 2016 javier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ PruebaRunator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaRunator
+//
+//  Created by javier on 5/13/16.
+//  Copyright (c) 2016 javier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
